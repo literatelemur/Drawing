9d0f3d2bc9790d53c78c536c2c500ce128a51bab
literatelemur <lukecarrington307@gmail.com>
2022-11-12 16:13:55 -0600

    Add files via upload

---
+++ Lab1CarringtonLuke/src/Drawing.java
@@ -0,0 +1,35 @@
+
+/*
+ This is a simple Java program that prints a "picture" on the screen.
+*/ 
+public class Drawing {
+    public static void main(String[] args) { 
+        System.out.println("                o"); 
+        System.out.println("                | [---]");
+        System.out.println("                |   |");
+        System.out.println("                |   |");
+        System.out.println("          /-----|---|\\");
+        System.out.println("         /___/____\\___\\");
+        System.out.println("         |            |");
+        System.out.println("         |            |"); 
+        System.out.println("         \\------------/");
+        System.out.println("          \\   \\  /   /");
+        System.out.println("      --------------------");
+        System.out.println("      |       NASA       |");
+        System.out.println("      --------------------");
+        System.out.println("        /     /  \\     \\");
+        System.out.println("       /      ----      \\");
+        System.out.println("       ^^              ^^");
+        System.out.println("  ________________________");
+        System.out.println("  |*******_______________|");
+        System.out.println("  |*******_______________|");
+        System.out.println("  |*******_______________|");
+        System.out.println("  |______________________|");
+        System.out.println("  |______________________|");
+        System.out.println("  |");
+        System.out.println("  |");
+        System.out.println("  |");
+        System.out.println("  |");
+        System.out.println("  |");
+    }
+}
